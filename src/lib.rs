3c33408d71847d77374a104743dc44c354bfd93a
Who5673 <windows7modrenedition@gmail.com>
2025-08-17 16:16:21 +0700

    website commit

---
+++ src/lib.rs
@@ -0,0 +1,8 @@
+use wasm_bindgen::prelude::*;
+/*use wasm_bindgen::JsCast;
+use web_sys::{HtmlCanvasElement, HtmlImageElement, CanvasRenderingContext2d, FileReader, HtmlInputElement};*/
+
+#[wasm_bindgen]
+pub fn returnformat(a: String) -> String {
+    a
+}
