9fdf35bb60cca824b2a74a6bf70cbd1f2795beec
Who5673 <windows7modrenedition@gmail.com>
2025-08-18 14:27:21 +0700

    website commit

--- docs/frontend.js
+++ docs/frontend.js
@@ -21,48 +21,45 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 */
-// import init, { returnformat } from "./rust-wasm/my_wasm_project.js";
+import init, { returnformat } from "./rust-wasm/my_wasm_project.js";
 
-// await init();
+await init();
 
 // WebAssembly and frontend.js
-Module.onRuntimeInitialized = function() {
-  const returnformat = Module.cwrap('returnformat', 'string', ['string']);
-  const result2 = returnformat("Hello from WebAssembly!");
-  console.log(result2);
-  let current_background = returnformat("dark");
+const result2 = returnformat("Hello from WebAssembly!");
+console.log(result2);
+let current_background = returnformat("dark");
 
-  /* Smooth scrolling */
-  window.addEventListener("DOMContentLoaded", () => {
-    const hash = window.location.hash;
-    if (hash) {
-      const target = document.querySelector(hash);
-      if (target) {
-        setTimeout(() => {
-          target.scrollIntoView({ behavior: "smooth" });
-        }, 100)
-      }
+/* Smooth scrolling */
+window.addEventListener("DOMContentLoaded", () => {
+  const hash = window.location.hash;
+  if (hash) {
+    const target = document.querySelector(hash);
+    if (target) {
+      setTimeout(() => {
+        target.scrollIntoView({ behavior: "smooth" });
+      }, 100)
     }
-  });
+  }
+});
 
-  /* Static scripts */
-  const backgroundmode = document.getElementById("light");
-  const menu = document.getElementsByTagName("nav")[0];
-  backgroundmode.addEventListener("click", () => {
-    switch (current_background) {
-      case "dark":
-        document.getElementById("bodie").classList.add("light");
-        menu.classList.add(returnformat("light"));
-        current_background = returnformat("light");
-        break;
-      default:
-        document.getElementById("bodie").classList.remove("light");
-        menu.classList.remove(returnformat("light"));
-        current_background = returnformat("dark");
-        break;
-    }
-  });
-}
+/* Static scripts */
+const backgroundmode = document.getElementById("light");
+const menu = document.getElementsByTagName("nav")[0];
+backgroundmode.addEventListener("click", () => {
+  switch (current_background) {
+    case "dark":
+      document.getElementById("bodie").classList.add("light");
+      menu.classList.add(returnformat("light"));
+      current_background = returnformat("light");
+      break;
+    default:
+      document.getElementById("bodie").classList.remove("light");
+      menu.classList.remove(returnformat("light"));
+      current_background = returnformat("dark");
+      break;
+  }
+});
 
 const currentPath = window.location.pathname;
   /* Get the boxes in Home */
