7ae9640b28be8650e09dfdee94e49486f3ad4e6e
Who5673 <windows7modrenedition@gmail.com>
2025-08-24 12:27:45 +0700

    website commit

---
+++ docs/deb/repo/repo-build.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+for d in stable kali-rolling bookworm artful bionic bullseye buster cosmic disco focal groovy hirsute impish jammy jessie kinetic noble oracular precise quantal raring saucy sid stretch trusty utopic vivid wheezy wily xenial yakkety zesty plucky; do
+  reprepro -b . includedeb "$d" password-generator_1.0.8_amd64.deb
+done
