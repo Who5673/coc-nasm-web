590d088e9b487b4ca865f7ca9e620a1a3f841cf5
Who5673 <windows7modrenedition@gmail.com>
2025-08-26 22:04:31 +0700

    website commit

--- docs/deb/repo/repo-build.sh
+++ docs/deb/repo/repo-build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 for d in stable kali-rolling bookworm artful bionic bullseye buster cosmic disco focal groovy hirsute impish jammy jessie kinetic noble oracular precise quantal raring saucy sid stretch trusty utopic vivid wheezy wily xenial yakkety zesty plucky; do
-  reprepro -b . includedeb "$d" ./nasm-to-wasm_$(nasm-to-wasm -v)_amd64.deb
+  reprepro -b . includedeb "$d" ./nasm-to-wasm_0.13.4_amd64.deb
 done
